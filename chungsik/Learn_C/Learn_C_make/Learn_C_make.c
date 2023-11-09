50703b28f67be674e88be41923d8190aefd2b39b
ppatabox <masin556@gmail.com>
2023-11-10 00:27:05 +0900

    :hammer: del : [23.11.10] [ppatabox] pull changed? error

---
+++ chungsik/Learn_C/Learn_C_make/Learn_C_make.c
@@ -0,0 +1,12 @@
+﻿// Learn_C_make.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "Learn_C_make.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
--- chungsik/c_basics/Struct/Struct.h
+++
@@ -1,7 +0,0 @@
-// t_point 구조체 정의
-typedef struct {
-    int x;
-    int y;
-    char str[50];
-} t_point;
-
